7a4285cc8c218ed6aa7373f55d252e30f7d1263f
Ben Kim <benkim2284@gmail.com>
2024-09-12 14:53:25 -0400

    connect

--- ProjectFoodFinder/urls.py
+++ ProjectFoodFinder/urls.py
@@ -1,6 +1,6 @@
 """
 URL configuration for ProjectFoodFinder project.
-
+hi
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
