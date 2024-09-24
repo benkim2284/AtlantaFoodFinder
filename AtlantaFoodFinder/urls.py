ebd56bd06c431af0dfcd1be8332d27ec49fa7b48
Ben Kim <benkim2284@gmail.com>
2024-09-24 14:37:14 -0400

    changed path url directories

--- AtlantaFoodFinder/urls.py
+++ AtlantaFoodFinder/urls.py
@@ -4,8 +4,9 @@ from . import views
 app_name = "AtlantaFoodFinder"
 
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
+    path("", views.home_view, name="index"),
+    path("map/", views.map_view, name="map"),
     path('register/', views.register, name='register'),  # Route for register view
     path('login/', views.login_view, name='login'),  # Route for login view
-    path('home/', views.home_view, name='home'),  # Route for login view
+    path('wassup/', views.wassup_view, name='wassup'),  # Route for login view
 ]
--- AtlantaFoodFinder/views.py
+++ AtlantaFoodFinder/views.py
@@ -12,61 +12,8 @@ from .forms import RegisterForm
 from django.contrib.auth.models import User
 
 
-
-class IndexView(generic.ListView):
-    template_name = "AtlantaFoodFinder/index.html"
-    context_object_name = "latest_question_list"
-
-    def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
-
-
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = "AtlantaFoodFinder/detail.html"
-
-    def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
-        return Question.objects.filter(pub_date__lte=timezone.now())
-
-
-class ResultsView(generic.DetailView):
-    model = Question
-    template_name = "AtlantaFoodFinder/results.html"
-
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST["choice"])
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(
-            request,
-            "AtlantaFoodFinder/detail.html",
-            {
-                "question": question,
-                "error_message": "You didn't select a choice.",
-            },
-        )
-
-    else:
-        selected_choice.votes = F("votes") + 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse("AtlantaFoodFinder:results", args=(question.id,)))
-
-
 def map_view(request):
-    context = {
-        'lat': -25.363,
-        'lng': 131.044
-    }
-    return render(request, 'map.html', context)
+    return render(request, 'AtlantaFoodFinder/index.html')
 
 
 def register(request):
@@ -88,18 +35,23 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('AtlantaFoodFinder:home')
+            return redirect('AtlantaFoodFinder:map')
         else:
             return render(request, 'AtlantaFoodFinder/login.html', {'error': 'Invalid credentials'})
     else:
         return render(request, 'AtlantaFoodFinder/login.html')
 
 
-def home_view(request):
+def wassup_view(request):
     # Check if the user is authenticated
     if request.user.is_authenticated:
         username = request.user.username  # Get the username of the logged-in user
-        return render(request, 'AtlantaFoodFinder/home.html', {'username': username})
+        return render(request, 'AtlantaFoodFinder/wassup.html', {'username': username})
     else:
         # Redirect to login if user is not logged in
         return redirect('login')
+
+def home_view(request):
+    # Check if the user is authenticated
+    return render(request, 'AtlantaFoodFinder/home.html')
+
