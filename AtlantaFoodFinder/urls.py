76abbb00294de7bca7b0e3ee982d7238ac1a7a9b
Aaron Fan <ailun.aaron.fan@gmail.com>
2024-09-27 15:18:15 -0400

    Added login, register, and password reset views and pages

--- AtlantaFoodFinder/urls.py
+++ AtlantaFoodFinder/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from . import views
 app_name = "AtlantaFoodFinder"
 
 urlpatterns = [
+    #comment
     path("", views.home_view, name="index"),
     path("map/", views.map_view, name="map"),
     path('register/', views.register, name='register'),  # Route for register view
     path('login/', views.login_view, name='login'),  # Route for login view
+    path('logout/', views.logout_view, name='logout'),
+    path('password_reset/', views.password_reset_view, name='password_reset'),
     path('wassup/', views.wassup_view, name='wassup'),  # Route for login view
 ]
