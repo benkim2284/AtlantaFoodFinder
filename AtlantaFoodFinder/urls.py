a2d094f5bb2d8126f044ac1bc0caf9623b73b72b
ChandlerChen <Chandler.ZX.Chen@gmail.com>
2024-09-30 19:52:35 -0400

    homepge

--- AtlantaFoodFinder/urls.py
+++ AtlantaFoodFinder/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "AtlantaFoodFinder"
 
 urlpatterns = [
-    path("", views.home_view, name="index"),
+    path("", views.home_view, name="home"),
     path("map/", views.map_view, name="map"),
     path('register/', views.register, name='register'),  # Route for register view
     path('login/', views.login_view, name='login'),  # Route for login view
