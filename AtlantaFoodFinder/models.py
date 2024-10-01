68ef48a9e558fe666168bd7fb3016f610b81cd6c
Ben Kim <benkim2284@gmail.com>
2024-09-30 21:45:15 -0400

    removed comments

--- AtlantaFoodFinder/models.py
+++ AtlantaFoodFinder/models.py
@@ -6,7 +6,6 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
-# Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
